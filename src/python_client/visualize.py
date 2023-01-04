55ae93e88ff8c57cffb8b18b7c5d64374de642e6
parisa <prisashahbi@gmail.com>
2023-01-04 20:17:33 +0330

    final changes

--- src/python_client/visualize.py
+++ src/python_client/visualize.py
@@ -14,14 +14,10 @@ for i in range(epochs):
     reward_history[i] = reward_episode
     total_reward_history[i] = total_reward
 
-
-segment = 10
 fig, axes = plt.subplots(2, 1, figsize=(5, 4), dpi=200, sharex='all')
-axes[0].plot(np.arange(len(total_reward_history)), total_reward_history, alpha=0.7, color='#d62728')
+axes[0].plot(np.arange(len(total_reward_history)), total_reward_history, alpha=0.7, color='yellowgreen')
 axes[0].set_ylabel('Total rewards')
-axes[0].legend(loc='best')
-axes[1].plot(np.arange(len(reward_history)), reward_history, marker='o', markersize=2, alpha=0.7, color='#2ca02c',
-             linestyle='none')
+axes[1].plot(np.arange(len(reward_history)), reward_history, marker='o', markersize=2, alpha=0.7, color='tomato', linestyle='none')
 axes[1].set_xlabel('Episode')
 axes[1].set_ylabel('Reward from\na single game')
 axes[1].xaxis.set_major_formatter(FormatStrFormatter('%.0f'))
